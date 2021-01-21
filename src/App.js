bfea83e8e28cb1cdd8b360bbff3f9747715734e9
Mizan <mizanrifat001@gmail.com>
2021-01-21 20:36:04 +0600

    Appbar_Added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import Appbar from './components/Appbar'
 
 function App() {
   return (
-    <div className="App">
-      Hello world
-    </div>
+    <>
+      <Appbar />
+    </>
   );
 }
 
